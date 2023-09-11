4ebaca3a1832bae969745b3f24e54fec3e8d9989
Johana-RHP <johana.rosicel@gmail.com>
2023-09-10 17:26:26 -0700

    feat: Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Log stats"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    MONGO_URI = "mongodb://localhost:27017"
+    client = MongoClient(MONGO_URI)
+    db = client['logs']
+    col = db['nginx']
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f'{col.count:documents({})} logs')
+    print('Methods:')
+    for method in methods:
+        count_method = db_nginx.count_documents({'method': method})
+        print(f'\tmethod {method}: {count_method}')
+
+    sc = "status check"
+    print(f"{col.count_documents({'method': 'GET','path': '/status'})} {sc}")
