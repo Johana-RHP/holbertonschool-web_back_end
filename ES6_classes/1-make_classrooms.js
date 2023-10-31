961486165113be4e473a9fee7d054937f422ff43
Johana-RHP <johana.rosicel@gmail.com>
2023-10-30 21:01:22 -0700

    fix

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,12 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  const rooms = [19, 20, 34].map((size) => new ClassRoom(size));
-  return rooms;
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
 }
 
 export default initializeRooms;
+
