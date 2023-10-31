bed317fe9cb3718857ac131bce7319cb2a62e491
Johana-RHP <johana.rosicel@gmail.com>
2023-10-30 21:38:46 -0700

    fix

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,39 +1,12 @@
-// HolbertonClass Class
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 
-export default class HolbertonClass {
-  constructor(size = '', location = '') {
-    this.size = size;
-    this.location = location;
-  }
-
-  [Symbol.toPrimitive](dataType) {
-    if (dataType === 'string') {
-      return (`${this.location}`);
-    } if (dataType === 'number') {
-      return (`${this.size}`);
-    }
-    return (`${this.location}`);
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('size must be a number');
-    }
-    this._size = value;
-  }
-
-  get location() {
-    return this._code;
-  }
-
-  set location(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('size must be a string');
-    }
-    this._code = value;
-  }
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
