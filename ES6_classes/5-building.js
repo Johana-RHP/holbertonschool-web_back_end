03e67866573c829de7335b90109d1e49bc8ad2b8
Johana-RHP <johana.rosicel@gmail.com>
2023-10-30 21:22:47 -0700

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,14 @@
-class Building {
-  constructor(sqft) {
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
   }
 
-  // Getter for 'sqft'
   get sqft() {
     return this._sqft;
   }
-
-  // Abstract method that must be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
-  }
 }
-
-export default Building;
-
