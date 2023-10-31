7607796248d62d2c3b6cc9f8dca3497531ea2047
Johana-RHP <johana.rosicel@gmail.com>
2023-10-30 21:37:40 -0700

    fix

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,23 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  // Getter for 'name'
-  get name() {
-    return this._name;
-  }
-
-  // Getter for 'code'
-  get code() {
-    return this._code;
-  }
-
-  // Override the default string description
   toString() {
-    return this._code; // Return the airport code
+    return `[object ${this._code}]`;
   }
 }
-
-export default Airport;
